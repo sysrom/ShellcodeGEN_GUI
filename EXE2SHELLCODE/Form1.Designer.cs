1e58d538f269cb4053dbc284510e5e419a5091fe
sysrom <88297660+sysrom@users.noreply.github.com>
2023-01-15 19:56:27 +0800

    Upload
    Just Upload

---
+++ EXE2SHELLCODE/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿
+namespace EXE2SHELLCODE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(25, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(101, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Donut转换方式";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(141, 12);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(107, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "pe2shc转换方式";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(115, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(314, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "要被转换的文件:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(66, 74);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "转换到:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(115, 71);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(314, 21);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(435, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "从磁盘选中文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(435, 71);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "从磁盘选中文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(386, 98);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(124, 58);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "火速拉出来";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 98);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(347, 48);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Donut方式转换的转换对.NET的兼容性较好\r\n若是用于大灰狼或者Ghost祖传衍生远控用pe2shc方式兼容性较好\r\n还有pe2shc不支持.net的程序\r\n" +
+    "By sysR@M|Project Start AT 01/15/2023";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(435, 5);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "点我当傻逼";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(511, 159);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "shellcode转换器 By sysROM";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ EXE2SHELLCODE/Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace EXE2SHELLCODE
+{
+    /*
+    ____           _______  _______ ____  ____  __  ___
+   / __ )__  __   / ___/\ \/ / ___// __ \/ __ \/  |/  /
+  / __  / / / /   \__ \  \  /\__ \/ /_/ / / / / /|_/ / 
+ / /_/ / /_/ /   ___/ /  / /___/ / _, _/ /_/ / /  / /  
+/_____/\__, /   /____/  /_//____/_/ |_|\____/_/  /_/   
+      /____/                                           
+    */
+    public partial class Form1 : Form
+    {
+        public String needcovPath;
+        public String OutputPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog nt = new OpenFileDialog()) {
+                nt.Title = "选择要转换的文件";
+                nt.Multiselect = false;
+                if (nt.ShowDialog() == DialogResult.OK) {
+                    needcovPath = nt.FileName;
+                    textBox1.Text = nt.FileName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (SaveFileDialog op = new SaveFileDialog())
+            {
+                op.Title = "输出到的文件";
+                op.Filter = ".bin (*.bin)|*.bin";
+                op.FileName = "Client";
+                if (op.ShowDialog() == DialogResult.OK)
+                {
+                    OutputPath = op.FileName;
+                    textBox2.Text = op.FileName;
+                }
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (needcovPath == null || OutputPath == null|| needcovPath ==""||OutputPath=="") {
+                MessageBox.Show("傻逼,请选择转换的文件或者转换到的文件");
+                return;
+            }
+            if (radioButton1.Checked) {
+                // MessageBox.Show("Donut方式");
+                string Donutpath = Path.Combine(Application.StartupPath, @"donut.exe");
+                if (!File.Exists(Donutpath))
+                {
+                    File.WriteAllBytes(Donutpath, Properties.Resources.donut);
+                }
+                
+                Process Process = new Process();
+                Process.StartInfo.FileName = Donutpath;
+                Process.StartInfo.CreateNoWindow = true;
+                Process.StartInfo.Arguments = "-f \"" + needcovPath + "\" -o \"" + OutputPath + "\"";
+                Process.Start();
+                Process.WaitForExit();
+                Process.Close();
+                File.Delete(Donutpath);
+                MessageBox.Show("生成成功!    方式:Donut\n"+"被转换的文件:"+needcovPath+"\n到文件:"+OutputPath);
+
+            } else if (radioButton2.Checked) {
+                // MessageBox.Show("pe2shc方式");
+                string pe2shcpath = Path.Combine(Application.StartupPath, @"pe2shc.exe");
+                if (!File.Exists(pe2shcpath))
+                {
+                    File.WriteAllBytes(pe2shcpath, Properties.Resources.pe2shc);
+                }
+
+                Process Process = new Process();
+                Process.StartInfo.FileName = pe2shcpath;
+                Process.StartInfo.CreateNoWindow = true;
+                Process.StartInfo.Arguments = " \"" + needcovPath + "\" \"" + OutputPath + "\"";
+                Process.Start();
+                Process.WaitForExit();
+                Process.Close();
+                File.Delete(pe2shcpath);
+                MessageBox.Show("生成成功!    方式:pe2shc\n" + "被转换的文件:" + needcovPath + "\n到文件:" + OutputPath);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("恭喜你是个傻逼");
+        }
+    }
+}
